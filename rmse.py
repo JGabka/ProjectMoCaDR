359415a6367c6a9c1c241bc8fe3e4d1cb1ccfda9
JGabka <jgabka8@gmail.com>
2025-04-28 11:40:59 +0200

    rmse

--- rmse.py
+++ rmse.py
@@ -11,3 +11,13 @@ def calculate_rmse(test_file, predictions):
     rmse = np.sqrt(np.mean((y_true - y_pred) ** 2))
     return rmse
 
+def calculate_rmse_sgd(train_file, Z_approx):
+  Z, user_map, movie_map = build_rating_matrix(train_file,'zero')
+  I=set(zip(*np.nonzero(Z)))
+  train_set, test_set = split_indices(I, seed=5)
+  rows = torch.tensor([i[0] for i in test_set], device=device)
+  cols = torch.tensor([i[1] for i in test_set], device=device)
+  Z = torch.from_numpy(Z)
+  loss = torch.mean((Z[rows, cols] - Z_approx[rows, cols]) ** 2)
+  rmse=loss**(1/2)
+  return rmse
